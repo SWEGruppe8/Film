d1ceeb0af91b12c021dfb732721cd14b6789c2b0
Dinh Thien Phan <dithienphan@gmail.de>
2022-04-18 15:01:52 +0200

    logger

---
+++ src/logger/response-time.interceptor.ts
@@ -0,0 +1,45 @@
+import {
+    type CallHandler,
+    type ExecutionContext,
+    Injectable,
+    type NestInterceptor,
+} from '@nestjs/common';
+import { type Observable } from 'rxjs';
+import { type Response } from 'express';
+import { Temporal } from '@js-temporal/polyfill';
+import { getLogger } from './logger.js';
+import { tap } from 'rxjs/operators';
+
+/**
+ * `ResponseTimeInterceptor` protokolliert die Antwortzeit und den Statuscode
+ * Alternative zu morgan (von Express) http://expressjs.com/en/resources/middleware/morgan.html,
+ * aber mit konformem Log-Layout.
+ */
+@Injectable()
+export class ResponseTimeInterceptor implements NestInterceptor {
+    readonly #logger = getLogger(ResponseTimeInterceptor.name);
+
+    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+        const start = Temporal.Now.instant().epochMilliseconds;
+        return next.handle().pipe(
+            tap(() => {
+                const response = context.switchToHttp().getResponse<Response>();
+                const { statusCode, statusMessage } = response;
+                const responseTime =
+                    Temporal.Now.instant().epochMilliseconds - start;
+                // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+                if (statusMessage === undefined) {
+                    // GraphQL
+                    this.#logger.debug('Response time: %d ms', responseTime);
+                    return;
+                }
+                this.#logger.debug(
+                    'Response time: %d ms, %d %s',
+                    responseTime,
+                    statusCode,
+                    statusMessage,
+                );
+            }),
+        );
+    }
+}
